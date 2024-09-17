ea58043d841fa70991973f3454bef6d4bb38393f
Ankit0607 <ankit.mishra06072000@gmail.com>
2024-09-17 22:09:06 +0530

    Learing DevOps first Commit

---
+++ src/main/java/com/springboot/docker/SpringBootDockerApplication.java
@@ -0,0 +1,23 @@
+package com.springboot.docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringBootDockerApplication {
+	
+	@GetMapping("/message")
+	public String getmessage() {
+		return "Helloo Docker !!";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDockerApplication.class, args);
+		
+		
+	}
+
+}
