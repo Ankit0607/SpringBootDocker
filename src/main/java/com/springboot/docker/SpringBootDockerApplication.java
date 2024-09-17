b7600aedbb0b596821d57d74b334e889824ab0c7
Ankit0607 <ankit.mishra06072000@gmail.com>
2024-09-17 22:17:06 +0530

    added code for jenkin testing

--- src/main/java/com/springboot/docker/SpringBootDockerApplication.java
+++ src/main/java/com/springboot/docker/SpringBootDockerApplication.java
@@ -1,20 +1,30 @@
 package com.springboot.docker;
 
+import org.apache.logging.log4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.annotation.PostConstruct;
+
 @SpringBootApplication
 @RestController
 public class SpringBootDockerApplication {
 	
-	@GetMapping("/message")
-	public String getmessage() {
-		return "Helloo Docker !!";
-	}
+//	@GetMapping("/message")
+//	public String getmessage() {
+//		return "Helloo Docker !!";
+//	}
+	public static Logger logger = (Logger) LoggerFactory.getLogger(SpringBootDockerApplication.class);
 
+	@PostConstruct
+	public void intt() {
+		logger.info("Application Satrted...");
+	}
 	public static void main(String[] args) {
+		logger.info("Application executed...");
 		SpringApplication.run(SpringBootDockerApplication.class, args);
 		
 		
--- src/test/java/com/springboot/docker/SpringBootDockerApplicationTests.java
+++ src/test/java/com/springboot/docker/SpringBootDockerApplicationTests.java
@@ -1,13 +1,21 @@
 package com.springboot.docker;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringBootDockerApplicationTests {
 
+	public static Logger logger = (Logger) LoggerFactory.getLogger(SpringBootDockerApplication.class);
+
 	@Test
 	void contextLoads() {
+		logger.info("test executed...");
+		assertEquals(true, true);
 	}
 
 }
